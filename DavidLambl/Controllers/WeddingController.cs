20ada2e2f9237fe8379e03ede34b1fafd46ee76d
David Lambl <david.lambl@msn.com>
2015-10-21 23:28:47 -0400

    added wedding photos page

---
+++ DavidLambl/Controllers/WeddingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DavidLambl.Controllers
+{
+    public class WeddingController : Controller
+    {
+        // GET: Wedding
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
