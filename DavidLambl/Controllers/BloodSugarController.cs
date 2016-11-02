257da77c8a3b9d71c3c1ae4b1cf927f443ccfbc9
David Lambl <david.lambl@msn.com>
2016-11-01 23:31:15 -0400

    updating to vs solution

---
+++ DavidLambl/Controllers/BloodSugarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DavidLambl.Controllers
+{
+    public class BloodSugarController : Controller
+    {
+        //
+        // GET: /BloodSugar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
